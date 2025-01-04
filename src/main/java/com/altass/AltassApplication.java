37fa217d31e3154d8933eb9ed1b94598fc0352e3
Lognath Paramashivam <lognathparamashivam@gmail.com>
2025-01-04 10:46:06 +0530

    Initial commit

---
+++ src/main/java/com/altass/AltassApplication.java
@@ -0,0 +1,13 @@
+package com.altass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altass/AltassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
